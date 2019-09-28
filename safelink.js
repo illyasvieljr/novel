999b8f9762f9c83336e2c49d494b687356e37851
illyasvieljr <32120029+illyasvieljr@users.noreply.github.com>
2019-09-28 16:13:06 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -19,12 +19,9 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*1)+ 1);
 var xxx=null;
-if(x=="1"){xxx="black-desert-online.html"}
-if(x=="2"){xxx="monster-hunter-world.html"}
-if(x=="3"){xxx="sword-art-online-fatal-bullet.html"}
-if(x=="4"){xxx="dragon-nest.html"}a_to_ve[i].href="https://blog.noveliar.com/2018/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="menggabungkan-mod-dragonnest.html"}a_to_ve[i].href="https://read.dnmodex.com/2019/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
